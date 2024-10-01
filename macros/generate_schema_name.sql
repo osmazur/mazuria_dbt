4b80c08b736f583e6ae17421dc51bc89c9962f12
Oleksandr Mazur <olem.mazur@gmail.com>
2024-10-01 12:15:55 +0000

    rename the schemas

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -3,15 +3,15 @@
     {%- set default_schema = target.schema -%}
 
     {%- if target.name == 'prod' -%}
-        mazuria_dbt_prod_{{ custom_schema_name | trim }}
+        dbt_prod_{{ custom_schema_name | trim }}
     {%- elif target.name == 'uat' -%}
-        mazuria_dbt_uat_{{ custom_schema_name | trim }}
+        dbt_uat_{{ custom_schema_name | trim }}
     {%- elif target.user == 'oleksandrmazur' -%} prod_ci_job
-        mazuria_dbt_{{target.user}}
+        dbt_{{target.user}}
     {%- elif target.name == 'prod_ci_job' -%} prod_ci_job
-        mazuria_dbt_prod_ci
+        dbt_prod_ci
     {%- elif target.name == 'uat_ci_job' -%} prod_ci_job
-        mazuria_dbt_uat_ci
+        dbt_uat_ci
     {%- else -%}
         {{ default_schema }}
     {%- endif -%}
