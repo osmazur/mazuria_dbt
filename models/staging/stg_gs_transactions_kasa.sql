9bef65b2590433527500f097ca929c86a2614bca
Oleksandr Mazur <mazur.ol.sr@gmail.com>
2026-06-02 20:15:39 +0300

    fix

--- models/staging/stg_gs_transactions_kasa.sql
+++ models/staging/stg_gs_transactions_kasa.sql
@@ -1,23 +1,24 @@
 
 with mazuria_gs_raw_transactions_kasa as (
 
-	select 
+	select
 		*
 	from {{ source('raw', 'mazuria_gs_raw_transactions_kasa') }}
+	where transaction_date is not null
 
 ),
 
 final as (
 
 	select
-        date(nullif(transaction_date, 'ʼ')) as transaction_date,
+        date(transaction_date) as transaction_date,
         operation_num::int as operation_num,
         quant_num::int as quant_num,
         sum_kasa,
         sum_added,
         sum::int as total_sum,
         'cash' as payment_type,
-        date(nullif(date_added, 'ʼ')) as date_added,
+        date(date_added) as date_added,
         source,
         is_income::boolean as is_income
 	from mazuria_gs_raw_transactions_kasa
